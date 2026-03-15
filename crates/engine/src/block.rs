73ee2f3d991c5cc55fba84c94c75a16a8e154f33
Jan Chojnacki <jan@chojnacki.dev>
2026-03-15 03:40:08 +0100

    formatted code

--- crates/engine/src/block.rs
+++ crates/engine/src/block.rs
@@ -1,5 +1,5 @@
-use thiserror::Error;
 use crate::speck_version::SpeckVersion;
+use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum BlockError {
--- crates/engine/src/key_iterator.rs
+++ crates/engine/src/key_iterator.rs
@@ -1,6 +1,6 @@
-use thiserror::Error;
 use crate::key::Key;
 use crate::speck_version::SpeckVersion;
+use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum KeyIteratorError {
