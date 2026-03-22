0fdd59d2fe8cb5a99dfef7447beb3f6a60b85bd5
Jan Chojnacki <jan@chojnacki.dev>
2026-03-22 01:58:36 +0100

    added multiversion search_range api poc

--- crates/engine/src/lib.rs
+++ crates/engine/src/lib.rs
@@ -3,6 +3,13 @@ pub mod backend;
 pub mod domain;
 
 use crate::api::request::{Operation, SearchRangeRequest};
+#[cfg(target_arch = "x86_64")]
+use crate::backend::avx2::engine::SearchEngineAVX2;
+#[cfg(target_arch = "x86_64")]
+use crate::backend::avx512::engine::SearchEngineAVX512;
+use crate::backend::scalar::engine::SearchEngineScalar;
+#[cfg(target_arch = "x86_64")]
+use crate::backend::sse2::engine::SearchEngineSSE2;
 use domain::block::BlockError;
 use domain::key::Key;
 use domain::key_iterator::KeyIteratorError;
@@ -31,4 +38,81 @@ pub trait SearchEngineBackend {
     fn search_range_decrypt(
         search_range_request: SearchRangeRequest,
     ) -> Result<Option<Vec<Key>>, SearchEngineBackendError>;
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, strum::Display)]
+pub enum SearchBackend {
+    Auto,
+    Scalar,
+    #[cfg(target_arch = "x86_64")]
+    SSE2,
+    #[cfg(target_arch = "x86_64")]
+    AVX2,
+    #[cfg(target_arch = "x86_64")]
+    AVX512,
+}
+
+impl Default for SearchBackend {
+    fn default() -> Self {
+        Self::Auto
+    }
+}
+
+pub fn search_range(
+    search_range_request: SearchRangeRequest,
+) -> Result<Option<Vec<Key>>, SearchEngineBackendError> {
+    search_range_with_backend(search_range_request, SearchBackend::Auto)
+}
+
+pub fn search_range_with_backend(
+    search_range_request: SearchRangeRequest,
+    backend: SearchBackend,
+) -> Result<Option<Vec<Key>>, SearchEngineBackendError> {
+    match backend {
+        SearchBackend::Auto => search_range_auto(search_range_request),
+        SearchBackend::Scalar => SearchEngineScalar::handle_request(search_range_request),
+        #[cfg(target_arch = "x86_64")]
+        SearchBackend::SSE2 => SearchEngineSSE2::handle_request(search_range_request),
+        #[cfg(target_arch = "x86_64")]
+        SearchBackend::AVX2 => {
+            if std::arch::is_x86_feature_detected!("avx2") {
+                SearchEngineAVX2::handle_request(search_range_request)
+            } else {
+                search_range_auto(search_range_request)
+            }
+        }
+        #[cfg(target_arch = "x86_64")]
+        SearchBackend::AVX512 => {
+            if std::arch::is_x86_feature_detected!("avx512f")
+                && std::arch::is_x86_feature_detected!("avx512bw")
+            {
+                SearchEngineAVX512::handle_request(search_range_request)
+            } else {
+                search_range_auto(search_range_request)
+            }
+        }
+    }
+}
+
+#[multiversion::multiversion(targets("x86_64+avx512f+avx512bw", "x86_64+avx2", "x86_64+sse2"))]
+fn search_range_auto(
+    search_range_request: SearchRangeRequest,
+) -> Result<Option<Vec<Key>>, SearchEngineBackendError> {
+    if multiversion::target::target_cfg_f!(all(
+        target_arch = "x86_64",
+        target_feature = "avx512f",
+        target_feature = "avx512bw"
+    )) {
+        return SearchEngineAVX512::handle_request(search_range_request);
+    }
+
+    if multiversion::target::target_cfg_f!(all(target_arch = "x86_64", target_feature = "avx2")) {
+        return SearchEngineAVX2::handle_request(search_range_request);
+    }
+
+    if multiversion::target::target_cfg_f!(all(target_arch = "x86_64", target_feature = "sse2")) {
+        return SearchEngineSSE2::handle_request(search_range_request);
+    }
+
+    SearchEngineScalar::handle_request(search_range_request)
 }
