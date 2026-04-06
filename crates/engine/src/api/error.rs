463b755acda9d2a21e446a9c00dcbe1735f06062
Jan Chojnacki <jan@chojnacki.dev>
2026-04-07 01:19:38 +0200

    deleted unused modules

--- crates/engine/src/api/error.rs
+++
@@ -1,8 +0,0 @@
-use crate::domain::block::BlockError;
-use thiserror::Error;
-
-#[derive(Debug, Error, Eq, PartialEq)]
-pub enum RequestError {
-    #[error(transparent)]
-    BlockError(#[from] BlockError),
-}
