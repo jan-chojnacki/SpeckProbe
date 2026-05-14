d3c6f254cfa911058b694fa9ceceeac740f6d3cb
Jan Chojnacki <jan@chojnacki.dev>
2026-05-14 23:24:53 +0200

    removed base package

---
+++ archlive/myiso/airootfs/root/rust.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export RUSTFLAGS="-C link-arg=-fuse-ld=mold -C target-cpu=native"
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=clang
+
+echo "Rust $(rustc --version)"
+echo "RUSTFLAGS=$RUSTFLAGS"
