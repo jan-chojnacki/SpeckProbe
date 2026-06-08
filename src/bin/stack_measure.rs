1a9680328680f91970e51f399a896e3682fd0f3a
Jan Chojnacki <jan@chojnacki.dev>
2026-06-08 12:38:25 +0200

    formatting

--- src/bin/stack_measure.rs
+++ src/bin/stack_measure.rs
@@ -1,3 +1,5 @@
+use speck_probe::speck;
+
 #[inline(never)]
 fn fn_addr<A, B, R>(f: unsafe fn(A, B) -> R) -> usize {
     f as usize
@@ -5,28 +7,28 @@ fn fn_addr<A, B, R>(f: unsafe fn(A, B) -> R) -> usize {
 
 fn main() {
     let _ = std::hint::black_box([
-        fn_addr(speck_probe::speck::scalar_encrypt_block_64_128),
-        fn_addr(speck_probe::speck::scalar_encrypt_block_inflight_64_128),
-        fn_addr(speck_probe::speck::scalar_decrypt_block_64_128),
+        fn_addr(speck::scalar_encrypt_block_64_128),
+        fn_addr(speck::scalar_encrypt_block_inflight_64_128),
+        fn_addr(speck::scalar_decrypt_block_64_128),
     ]);
 
     #[cfg(target_arch = "x86_64")]
     let _ = std::hint::black_box([
-        fn_addr(speck_probe::speck::sse2_encrypt_block_64_128),
-        fn_addr(speck_probe::speck::sse2_encrypt_block_inflight_64_128),
-        fn_addr(speck_probe::speck::sse2_decrypt_block_64_128),
-        fn_addr(speck_probe::speck::avx2_encrypt_block_64_128),
-        fn_addr(speck_probe::speck::avx2_encrypt_block_inflight_64_128),
-        fn_addr(speck_probe::speck::avx2_decrypt_block_64_128),
-        fn_addr(speck_probe::speck::avx512_encrypt_block_64_128),
-        fn_addr(speck_probe::speck::avx512_encrypt_block_inflight_64_128),
-        fn_addr(speck_probe::speck::avx512_decrypt_block_64_128),
+        fn_addr(speck::sse2_encrypt_block_64_128),
+        fn_addr(speck::sse2_encrypt_block_inflight_64_128),
+        fn_addr(speck::sse2_decrypt_block_64_128),
+        fn_addr(speck::avx2_encrypt_block_64_128),
+        fn_addr(speck::avx2_encrypt_block_inflight_64_128),
+        fn_addr(speck::avx2_decrypt_block_64_128),
+        fn_addr(speck::avx512_encrypt_block_64_128),
+        fn_addr(speck::avx512_encrypt_block_inflight_64_128),
+        fn_addr(speck::avx512_decrypt_block_64_128),
     ]);
 
     #[cfg(target_arch = "aarch64")]
     let _ = std::hint::black_box([
-        fn_addr(speck_probe::speck::neon_encrypt_block_64_128),
-        fn_addr(speck_probe::speck::neon_encrypt_block_inflight_64_128),
-        fn_addr(speck_probe::speck::neon_decrypt_block_64_128),
+        fn_addr(speck::neon_encrypt_block_64_128),
+        fn_addr(speck::neon_encrypt_block_inflight_64_128),
+        fn_addr(speck::neon_decrypt_block_64_128),
     ]);
 }
