50bf9a656d516b2775f66fb7bade8e83db0f39ad
Jan Chojnacki <jan@chojnacki.dev>
2026-06-18 20:24:48 +0200

    naming

--- tools/measure_memory.sh
+++ tools/measure_memory.sh
@@ -8,8 +8,8 @@ rm -f target/debug/deps/*.ll
 cargo rustc --lib -- --emit=llvm-ir -C opt-level=0 -C debuginfo=0 -C codegen-units=1 2>/dev/null
 cp target/debug/deps/speck_probe-*.ll /tmp/lib_o0.ll
 
-cargo rustc --bin stack_measure -- --emit=llvm-ir -C opt-level=0 -C debuginfo=0 -C codegen-units=1 2>/dev/null
-cp target/debug/deps/stack_measure-*.ll /tmp/bin_o0.ll
+cargo rustc --bin measure_memory -- --emit=llvm-ir -C opt-level=0 -C debuginfo=0 -C codegen-units=1 2>/dev/null
+cp target/debug/deps/measure_memory-*.ll /tmp/bin_o0.ll
 
 cat /tmp/lib_o0.ll /tmp/bin_o0.ll > ./output/o0.ll
 
@@ -18,7 +18,7 @@ rm -f target/debug/deps/*.ll
 cargo rustc --lib -- --emit=llvm-ir -C opt-level=3 -C debuginfo=0 -C codegen-units=1 2>/dev/null
 cp target/debug/deps/speck_probe-*.ll /tmp/lib_o3.ll
 
-cargo rustc --bin stack_measure -- --emit=llvm-ir -C opt-level=3 -C debuginfo=0 -C codegen-units=1 2>/dev/null
-cp target/debug/deps/stack_measure-*.ll /tmp/bin_o3.ll
+cargo rustc --bin measure_memory -- --emit=llvm-ir -C opt-level=3 -C debuginfo=0 -C codegen-units=1 2>/dev/null
+cp target/debug/deps/measure_memory-*.ll /tmp/bin_o3.ll
 
 cat /tmp/lib_o3.ll /tmp/bin_o3.ll > ./output/o3.ll
